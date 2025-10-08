52be59ffbc3811b16dbb48b8bd2f6df71b5a3e03
sameer4712 <sameerptmc@gmail.com>
2025-10-08 23:40:02 +0530

    2nd

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 function Button({name}) {
     return (
         <div>
+            
             <button className='bg-cyan-400 text-xl text-center font-bold flex py-2 pb-3 px-4 gap-3 border border-gray-400 text-gray-800 rounded-xl hover:bg-cyan-500 tansition-all duration-150 ease-in-out cursor-pointer '>
                 {name} 
             </button>
