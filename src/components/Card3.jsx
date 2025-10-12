abaee5cef5098580b77d6b9ab960d3600c78041c
sameer4712 <sameerptmc@gmail.com>
2025-10-12 23:38:41 +0530

    4th

--- src/components/Card3.jsx
+++ src/components/Card3.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 function Card3({no,title,para}) {
   return (
+    
     <div className='flex flex-col items-center justify-center py-9 px-17 text-cyan-50 bg-slate-900 lg:mt-4 border border-gray-700 rounded-lg'>
         <h1 className='font-bold text-4xl mb-2 '>{no}</h1>
         <h2  className='font-bold text-lg mb-2'>{title}</h2> 
